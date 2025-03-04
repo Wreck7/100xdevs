c34cb80ab1775584065ecc52f3dc53688aeb9a1d
Wreck7 <vishwagovula07@gmail.com>
2025-03-04 11:52:59 +0530

    learnt why DOM is headache

--- week4.2/app.js
+++ week4.2/app.js
@@ -1,18 +1,41 @@
-function addTodo() {
-    const title = document.getElementById('title').value
-    const description = document.getElementById('description').value
-    // const originalHTML = document.querySelector(".store").innerHTML;
-    // document.querySelector('.store').innerHTML = originalHTML + `
-    // <div>
-    // <div>${title}</div>
-    // <div>${description}</div>
-    // <button>mark as done</button>
-    // </div>`
-    const outerDiv = document.createElement('div');
-    // outerDiv.innerHTML = title;
-    const titleDiv = document.createElement('div').innerHTML = title;
-    // const descriptionDiv = document.createElement('div').innerHTML = description;
-    outerDiv.appendChild(titleDiv);
-    // outerDiv.appendChild(descriptionDiv);
-    document.querySelector('.store').appendChild(outerDiv);
+// let globalId = 1;
+// function addTodo() {
+//     const title = document.getElementById('title').value
+//     const description = document.getElementById('description').value
+//     document.querySelector('.store').appendChild(createChild(title, description, globalId++));
+// }
+
+function createChild(title, description, id) {
+    const child = document.createElement('div');
+    const firstChild = document.createElement('div')
+    firstChild.innerHTML = title;
+    const secondChild = document.createElement('div')
+    secondChild.innerHTML = description;
+    const thirdChild = document.createElement('button')
+    thirdChild.innerHTML = 'Mark as done';
+    thirdChild.setAttribute('onclick', `markAsDone(${id})`)
+    child.appendChild(firstChild);
+    child.appendChild(secondChild);
+    child.appendChild(thirdChild);
+    child.setAttribute('id', id)
+    return child
 }
+
+// function markAsDone(id) {
+//     const selectedTodo = document.getElementById(id);
+//     selectedTodo.children[2].innerHTML = 'Done!'
+// }
+
+function updateTodos(state) {
+    const parent = document.querySelector('.store')
+    parent.innerHTML = "";
+    for (let i = 0; i < state.length; i++) {
+        const child = createChild(state[i].title, state[i].description, state[i].id)
+        parent.appendChild(child)
+    }
+}
+window.setInterval(async function(){
+    const res = await fetch("http://localhost:3000/todos");
+    const json = await res.json()
+    updateTodos(json.todos)
+}, 3000);

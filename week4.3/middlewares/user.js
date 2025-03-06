3a2512c8f4b602106e64d9fe79878d3f725118e0
Wreck7 <vishwagovula07@gmail.com>
2025-03-06 23:35:29 +0530

    done with middlewares in assignment

--- week4.3/middlewares/user.js
+++ week4.3/middlewares/user.js
@@ -1,10 +1,10 @@
-const { Admin } = require('../db/index')
+const { User } = require('../db/index')
 function adminMiddleware(req, res, next) {
     const {username, password} = req.headers
-    const check = Admin.findOne({username: username})
+    const check = User.findOne({username: username})
     check.then(function(val) {
         if(!val){
-            res.status(403).json({message: "admin doesn't exists"})
+            res.status(403).json({message: "user doesn't exists"})
         }else{
             next()
         }

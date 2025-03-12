f550381ec28d088c838bb6f2046258728193f51e
Wreck7 <vishwagovula07@gmail.com>
2025-03-12 15:43:15 +0530

    completed assignment

--- week5.2/frontend/src/components/Todos.jsx
+++ week5.2/frontend/src/components/Todos.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 
-const Todos = ({todos}) => {
-  return (
-    <div>
-        {todos.map(function(todo){
-            return <div>
-            <h2>{todo.title}</h2>
-            <h3>{todo.description}</h3>
-            <button >{todo.completed == true ? "Completed" : "Mark as completed"}</button>
-            </div>
-        })}
-    </div>
-  )
+const Todos = ({ todos }) => {
+    function updateTodo(id) {
+        async function fetchAddTodo() {
+            const response = await fetch('http://localhost:3001/completed', {
+                method: "PUT",
+                body: JSON.stringify({
+                    id: id
+                }),
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+            const json = await response.json()
+        }
+        fetchAddTodo()
+    }
+    return (
+        <div>
+            {todos.map(function (todo) {
+                return <div>
+                    <h2>{todo.title}</h2>
+                    <h3>{todo.description}</h3>
+                    <button onClick={() => updateTodo(todo._id)}>{todo.completed == true ? "Completed" : "Mark as completed"}</button>
+                </div>
+            })}
+        </div>
+    )
 }
 
 export default Todos

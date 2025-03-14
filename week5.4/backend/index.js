55e67cee5ca70b2a93388671be546c997088f402
Wreck7 <vishwagovula07@gmail.com>
2025-03-14 23:33:13 +0530

    now its completely done fixed some errors

--- week5.4/backend/index.js
+++ week5.4/backend/index.js
@@ -14,14 +14,18 @@ app.post('/cards', async function (req, res) {
     const validate = userSchema.safeParse(req.body);
     if (!validate.success) {
         res.status(401).json({
-            message: 'incorrect data types entered!'
+            message: 'incorrect data types entered!',
+            errors: validate.error.errors.map(err => ({
+                path: err.path.join('.'),
+                message: err.message
+            }))
         })
     } else {
         await User.create({
-            name,
-            description,
-            interests,
-            socials
+            name: req.body.name,
+            description: req.body.description,
+            interests: req.body.interests,
+            socials: req.body.socials
         })
         res.json({
             message: 'successfully added card details'
@@ -33,5 +37,7 @@ app.get('/cards', async function(req,res){
     const response = await User.find({})
     res.json({cards: response})
 })
+
+app.listen(3002);
 
 
--- week5.4/backend/types.js
+++ week5.4/backend/types.js
@@ -1,7 +1,7 @@
 const z = require('zod');
 
 const userSchema = z.object({
-    username: z.string().min(3).max(20),
+    name: z.string().min(3).max(20),
     description: z.string(),
     interests: z.array(z.string()).min(1),
     socials: z.array(z.string()).min(1)

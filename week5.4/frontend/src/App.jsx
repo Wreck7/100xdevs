5a0143f1698b832954d691e9489c56ea5a966929
Wreck7 <vishwagovula07@gmail.com>
2025-03-14 23:55:56 +0530

    successfully fetched cards in frondend

--- week5.4/frontend/src/App.jsx
+++ week5.4/frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import Card from './compo/card'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 function App() {
-  const [cards, setcards] = useState([
-    {
-      name: "Vishwa Govula",
-      desc: "Budding full-stack dev",
-      interests: ["JavaScript", "HTML", "CSS", "TailwindCSS", "React JS", "Node JS", "Express JS", "MongoDB"],
-      socials: ["Twitter", "LinkedIN", "Instagram", "Peerlist"]
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    async function getCards() {
+      try {
+        const response = await fetch('http://localhost:3002/cards');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setCards(result.cards);
+      } catch (error) {
+        console.error('Failed to fetch cards:', error);
+      }
     }
-  ])
+
+    getCards();
+  }, []);
   return (
     <div className='main'>
       <Card cards={cards} />

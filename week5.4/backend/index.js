9746238940baebea508307c4234544bf5c65aadf
Wreck7 <vishwagovula07@gmail.com>
2025-03-14 23:19:51 +0530

    I think backend is completed!

--- week5.4/backend/index.js
+++ week5.4/backend/index.js
@@ -6,5 +6,32 @@ const { User } = require('./db')
 
 app.use(express.json());
 
+// function validate(req,res,next) {
+//     userSchema.safeParse
+// }
+
+app.post('/cards', async function (req, res) {
+    const validate = userSchema.safeParse(req.body);
+    if (!validate.success) {
+        res.status(401).json({
+            message: 'incorrect data types entered!'
+        })
+    } else {
+        await User.create({
+            name,
+            description,
+            interests,
+            socials
+        })
+        res.json({
+            message: 'successfully added card details'
+        })
+    }
+})
+
+app.get('/cards', async function(req,res){
+    const response = await User.find({})
+    res.json({cards: response})
+})
 
 

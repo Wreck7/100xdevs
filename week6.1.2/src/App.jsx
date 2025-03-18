1fdae3b51e0c9b4fbbe02ec71aa5fe05215441eb
Wreck7 <vishwagovula07@gmail.com>
2025-03-18 23:32:15 +0530

    done with keys react

---
+++ week6.1.2/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './compo/Todo'
+
+function App() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [todos, setTodos] = useState([
+    {title: 'go to GYM',
+      description: '7-8PM daily',
+      id: 1
+    },
+    {title: 'go to shop',
+      description: '8-9PM daily',
+      id: 2
+    },
+    {title: 'meet friends',
+      description: '9PM daily',
+      id: 3
+    },
+  ])
+  function addTodo(){
+    setTodos([...todos, {title, description, id: todos.length+1}])
+  }
+  return (
+    <div>
+      <input type="text" placeholder='Title' onChange={(e) => setDescription(e.target.value)}/>
+      <input type="text" placeholder='Description' onChange={(e) => setTitle(e.target.value)}/>
+      <button onClick={addTodo}>add todo</button>
+      {todos.map((todo)=> (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
+    </div>
+  )
+}
+
+export default App

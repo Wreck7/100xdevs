56e39725e8f57abb62ba198eccd7965df3678ed3
Wreck7 <vishwagovula07@gmail.com>
2025-03-20 23:00:35 +0530

    useEffect way of memoization

--- week6.2.2/frontend/src/App.jsx
+++ week6.2.2/frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [counter, setCounter] = useState(0)
   const [input, setInput] = useState(1)
-  let count = 0;
+  const [finalVal, setFinalVal] = useState(0)
+  useEffect(() => {
+    let count = 0;
   for (let i = 1; i <= input; i++) {
     count = count + i; 
   }
+  setFinalVal(count);
+  }, [input])
+
   return (
     <div>
       <input type="Number" onChange={(e) => setInput(e.target.value)} value={input} />
-      <div>sum is {count}</div>
+      <div>sum is {finalVal}</div>
       <button onClick={() => setCounter(counter + 1)}>counter({counter})</button>
     </div>
   )

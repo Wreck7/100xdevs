675d6f4a3070aa87beb602dfa804bb0a778a480d
Wreck7 <vishwagovula07@gmail.com>
2025-03-20 23:08:48 +0530

    optimal way to stop renders. memoization is useMemo

--- week6.2.2/frontend/src/App.jsx
+++ week6.2.2/frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
 
 function App() {
   const [counter, setCounter] = useState(0)
   const [input, setInput] = useState(1)
-  const [finalVal, setFinalVal] = useState(0)
-  useEffect(() => {
-    let count = 0;
-  for (let i = 1; i <= input; i++) {
-    count = count + i; 
-  }
-  setFinalVal(count);
+  let count = useMemo(() => {
+    let finalVal = 0;
+    for (let i = 1; i <= input; i++) {
+      finalVal = finalVal + i;
+    }
+    return finalVal
   }, [input])
 
   return (
     <div>
       <input type="Number" onChange={(e) => setInput(e.target.value)} value={input} />
-      <div>sum is {finalVal}</div>
+      <div>sum is {count}</div>
       <button onClick={() => setCounter(counter + 1)}>counter({counter})</button>
     </div>
   )

01797a43c57fc45d01791803664e76d2d3372c60
Wreck7 <vishwagovula07@gmail.com>
2025-03-29 23:23:44 +0530

    done useMemo recap!

--- week.6.3/src/App.jsx
+++ week.6.3/src/App.jsx
@@ -1,23 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import './App.css'
 
 function App() {
-  const [exchange, setExchange] = useState({})
+  const [exchange1, setExchange1] = useState({})
+  const [exchange2, setExchange2] = useState({})
   const [bank, setBank] = useState({})
+
+  useEffect(() => {
+    setExchange1({ returns: 100 });
+  }, [])
+
+  useEffect(() => {
+    setExchange2({ returns: 100 });
+  }, [])
+
   useEffect(() => {
     setTimeout(() => {
-      setExchange({returns: 100});
+      setBank({ amount: 100 });
     }, 1000);
   }, [])
-  useEffect(() => {
-    setTimeout(() => {
-      setBank({amount: 100});
-    }, 1000);
-  }, [])
-  const incomeTax = (exchange.returns + bank.amount) * 0.2;
+
+  const exchanges = useMemo(() => {
+    console.log("calculating")
+    return exchange1.returns + exchange2.returns
+  }, [exchange1, exchange2])
+
+  const incomeTax = (exchanges + bank.amount) * 0.2;
+
   return (
     <div>
-      your tax is {incomeTax}
+      your income tax is {incomeTax}
     </div>
   )
 }

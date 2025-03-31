18de72a382f5d56a63475712aa80dee8fc73c771
Wreck7 <vishwagovula07@gmail.com>
2025-03-31 11:41:47 +0530

    done assignment 4 useCallback

--- week6.3_assignments/src/App.jsx
+++ week6.3_assignments/src/App.jsx
@@ -3,14 +3,16 @@ import './App.css'
 import { Assign1 } from './compo/Assign1'
 import { Assign2 } from './compo/Assign2'
 import { Assign3 } from './compo/Assign3'
+import { Assign4 } from './compo/Assign4'
 
 function App() {
 
   return (
     <div>
-      <Assign1 />
+      {/* <Assign1 /> */}
       {/* <Assign2 /> */}
-      <Assign3 />
+      {/* <Assign3 /> */}
+      <Assign4 />
     </div>
   )
 }

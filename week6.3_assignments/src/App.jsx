6104a6b00b5b7e5f3afa80a5d0bb1722c28fa8b6
Wreck7 <vishwagovula07@gmail.com>
2025-03-31 11:15:47 +0530

    practiced assign1 and assign2

---
+++ week6.3_assignments/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import { Assign1 } from './compo/Assign1'
+import { Assign2 } from './compo/Assign2'
+
+function App() {
+
+  return (
+    <div>
+      <Assign1 />
+      <Assign2 />
+    </div>
+  )
+}
+
+export default App

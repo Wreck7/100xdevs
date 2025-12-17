e5a0fe0c273f5619ab4bd8b9e0857438ca5eaca4
Vishwa <vishwagovula07@gmail.com>
2025-12-17 21:18:26 +0530

    learnt some web-sockets basic

---
+++ web_sockets_prac/server/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+import express from "express";
+import { WebSocketServer } from "ws";
+
+const app = express();
+
+const port = 8080;
+
+const server = app.listen(port, () => {
+  console.log("server is live on 7070 port");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    console.log("data from client: %s", data);
+    ws.send("thanks bro!");
+  });
+});

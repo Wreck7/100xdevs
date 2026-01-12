0c7554a08c79962542a6d82ec96344102c62da8f
Vishwa <vishwagovula07@gmail.com>
2026-01-13 00:08:45 +0530

    learnt animations and transitions

--- week13.1/src/components/BasicProjectScreen.jsx
+++ week13.1/src/components/BasicProjectScreen.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const BasicProjectScreen = () => {
   return (
-    <div className='flex'>
-        <div className='bg-yellow-300 w-64 hidden md:flex h-screen'>sidebar</div>
-        <div className='bg-red-300 w-full h-screen'>content</div>
+    <div className="flex">
+      {/* <div className='bg-yellow-300 h-screen transition-all duration-700 w-0 md:w-64'>sidebar</div> */}
+      {/* in here it only push content section towards sidebar, no sidebar movement. */}
+      <div className="bg-yellow-300 h-screen transition-all duration-700 md:translate-x-0 -translate-x-64 w-0 md:w-64">
+        sidebar
+      </div>
+      <div className="bg-red-300 w-full h-screen">content</div>
     </div>
-  )
-}
+  );
+};
